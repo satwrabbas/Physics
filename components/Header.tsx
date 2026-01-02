f5df79a6728ff0394f9e12b103697ac5e37d168d
Abbas Satwr <satwrabbas@gmail.com>
2026-01-02 12:31:02 +0300

    Update header title and add logo image

--- components/Header.tsx
+++ components/Header.tsx
@@ -87,7 +87,12 @@ const Header: React.FC = () => {
   return (
     <header className="space-y-6">
       <div className="flex justify-between items-center">
-        <h1 className="text-3xl font-extrabold text-slate-800 dark:text-white"> تختيم الفيزيا</h1>
+        <img 
+      src="/logo.svg" 
+      alt="شعار التطبيق" 
+      className="w-8 h-8 sm:w-12 sm:h-12 object-contain" 
+    />
+        <h1 className="text-3xl font-extrabold text-slate-800 dark:text-white">رحلة إتقان الفيزياء</h1>
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
